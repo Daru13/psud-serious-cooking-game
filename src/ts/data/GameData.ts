f1c098b5278d216cd491b5df5125aaa6ad657424
Daru13 <daru14@gmail.com>
2020-02-20 03:30:45 +0100

    Add initially available recipes to the game data JSON.

--- src/ts/data/GameData.ts
+++ src/ts/data/GameData.ts
@@ -1,16 +1,18 @@
 import * as gameDataJSON from "./GameData.json";
 
 import { Ingredient } from './Ingredient';
-import { Recipe } from './Recipe';
+import { Recipe, RecipeName } from './Recipe';
 
 export class GameData {
     readonly ingredients: Ingredient[];
     readonly recipes: Recipe[];
+    readonly initiallyAvailableRecipeNames: RecipeName[];
 
     constructor() {
         this.ingredients = Array.from(gameDataJSON.ingredients)
             .map(Ingredient.fromSerialisedIngredient);
         this.recipes = Array.from(gameDataJSON.recipes)
             .map(Recipe.fromSerialisedRecipe);
+        this.initiallyAvailableRecipeNames = gameDataJSON.initiallyAvailableRecipes;
     }
 }
